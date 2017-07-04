b69ac246cae5916342edab166d99aff7d74c5b4e
Faresis <nikignatiev@gmail.com>
2017-07-04 08:28:28 +0300

    All dependencies are moved into Spring container.

--- src/main/java/ua/dp/mign/App.java
+++ src/main/java/ua/dp/mign/App.java
@@ -1,8 +1,18 @@
 package ua.dp.mign;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Calendar;
+
 public class App {
-    Client client;
-    EventLogger eventLogger;
+    private final Client client;
+    private final EventLogger eventLogger;
+
+    public App(Client client, EventLogger eventLogger) {
+        this.client = client;
+        this.eventLogger = eventLogger;
+    }
 
     private void logEvent(String message) {
         String formatted = message.replaceAll(client.getId(), client.getFullName());
@@ -10,10 +20,8 @@ public class App {
     }
 
     public static void main(String[] args) {
-        App app = new App();
-        app.client = new Client("1", "John Smith");
-        app.eventLogger = new ConsoleEventLogger();
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        App app = context.getBean(App.class);
         app.logEvent("Some event for user 1");
     }
 }
